9e4b40ef7bdce2946a605d66eb121837592e2125
cloud.liao <cloud.liao@gmail.com>
2016-03-03 16:28:19 +0800

    增加兩個測試站

--- src/stubs/after.sh
+++ src/stubs/after.sh
@@ -3,3 +3,9 @@
 # If you would like to do some extra provisioning you may
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
+
+cd %USER_PATH%
+cd Code/
+git clone 
+
+
